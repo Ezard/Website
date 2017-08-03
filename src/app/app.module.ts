42b9f9f2764573d10317ea8a95a2cff98ecc2bd2
Ezard <ezard.ben@gmail.com>
2017-08-03 19:18:54 +0100

    Create basic components and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MdSidenavModule, MdToolbarModule} from '@angular/material';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {ResumeComponent} from './resume/resume.component';
+import {ContactComponent} from './contact/contact.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        PortfolioComponent,
+        ResumeComponent,
+        ContactComponent
+    ],
+    imports: [
+        BrowserModule,
+        MdSidenavModule,
+        MdToolbarModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                component: HomeComponent
+            },
+            {
+                path: 'portfolio',
+                component: PortfolioComponent
+            },
+            {
+                path: 'resume',
+                component: ResumeComponent
+            },
+            {
+                path: 'contact',
+                component: ContactComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
